361606d928554511e8d80d02cf7215df4b7f0a9c
Eric Steedly <ericsteedly@gmail.com>
2024-03-12 11:58:40 -0500

    set up create categories button

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom"
 
 
 export default function CategoryList() {
+    const navigate = useNavigate()
   return (
-    <div>CategoryList</div>
+    <div className="categoryList-main-container">
+        <div className="create-category-btn-container">
+            <button onClick={navigate('./createCategory')}>Create Category</button>
+        </div>
+    </div>
   )
 }
