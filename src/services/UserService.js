4ab2adebe1b807cb490e5203ce00515304ba586d
Eric Steedly <ericsteedly@gmail.com>
2024-03-12 10:41:54 -0500

    corrected getUserByEmail query in fetch request

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,5 @@
 export const getUserByEmail = async (email) => {
-    return await fetch(`http://localhost:8088/users?email=${email}`).then(
+    return await fetch(`http://localhost:8088/users?user_email=${email}`).then(
         (res) => res.json()
     )
 }
