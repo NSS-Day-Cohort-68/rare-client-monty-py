fd2c0ce39016c161ba1cef648a4b1c79400271f6
Eric Steedly <ericsteedly@gmail.com>
2024-03-12 11:37:41 -0500

    Created CategoryList Component and Route

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,7 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import CategoryList from "../components/categories/CategoryList";
 
 
 export default function ApplicationViews() {
   return (
-    <div>Now Viewing ApplicationViews</div>
+    <Routes>
+      <Route path='categories' element={<CategoryList/>} />
+    </Routes>
   )
 }
