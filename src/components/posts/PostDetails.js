fbef842698f4896545362b764ff28bc07d3a0565
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-13 16:08:47 -0500

    Post detials displaying on webpage, except for post author

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -4,15 +4,41 @@ import { getPostById } from "../services/PostService";
 
 
 export const PostDetails = () => {
-    // const { postId } = useParams({});
-    // const { post, setPost} = useState({});
+    const { postId } = useParams({});
+    const [post, setPost] = useState({});
 
-// useEffect(() => {
-// getPostById(postId).then((data) => {
-// const postObj = data[0];
-// setPost(postObj)
-// })
-// }, [postId])
+useEffect(() => {
+getPostById(postId).then((data) => {
+const postObj = data[0];
+setPost(postObj);
+})
+}, [postId])
 
-return <><h1>Post Details</h1></>
+return (<section>
+    <div>
+    <h1>Post Details</h1>
+    </div>
+    <div>
+        <span>Post Title: </span>
+        {post.post_title}
+    </div>
+    <div>
+        <span>Post Image: </span>
+        {post.post_image_url}
+    </div>
+    <div>
+        <span>Content: </span>
+        {post.post_content}
+    </div>
+    <div>
+        <span>Date: </span>
+        {post.post_date}
+    </div>
+    {/* <div>
+        <span>Author: </span>
+        {post.post_author.author_first_name}
+    </div> */}
+
+    </section>
+)
 }
