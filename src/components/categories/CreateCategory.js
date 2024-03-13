d595c26ef22b49e8f8a4ef03aa3bd3e7f7999824
Eric Steedly <ericsteedly@gmail.com>
2024-03-13 15:19:08 -0500

    created basic category input form

--- src/components/categories/CreateCategory.js
+++ src/components/categories/CreateCategory.js
@@ -1,7 +1,36 @@
+import { useState } from "react"
 
 
 export default function CreateCategory() {
+  const [category, setCategory] = useState("")
+
+  const handleSave = (e) => {
+    e.preventDefault()
+    const categoryObj = {
+      "label": category
+    }
+
+    
+  }
+
+
   return (
-    <div>CreateCategory Here!</div>
+    <div className="createCategory-main">
+      <form className="createCategory-form" onSubmit={handleSave}>
+        <h1>Creat a New Category</h1>
+        <fieldset>
+          <input 
+            type="text"
+            placeholder="category"
+            required
+            value={category}
+            onChange={(e)=>{setCategory(e.target.value)}}
+          />
+        </fieldset>
+        <div className="createCategory-btn-container">
+          <button className="createCategory-btn" type="submit">Save</button>
+        </div>
+      </form>
+    </div>
   )
 }
