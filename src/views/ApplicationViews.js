51bb9c148044686b25108138259bf1ce1068e90a
Will MacLean <maclean.will@gmail.com>
2024-03-13 15:46:32 -0500

    added route

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,9 +1,23 @@
 import { Navbar } from "../components/navbar/Navbar"
+import { Route, Routes, Outlet } from "react-router-dom"
+import { CurrentUserPosts } from "../components/posts/CurrentUserPosts"
 
 export default function ApplicationViews() {
   return (
-    <div>
-    < Navbar/>
-    </div>
+    <Routes> 
+            <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Outlet />
+              </>
+            }
+            >
+            {/* <Route path="createpost" element={<CreatePost/>} /> */}
+            <Route path="myposts" element={<CurrentUserPosts/>} />
+            </Route>
+    </Routes>
+  
   )
 }
