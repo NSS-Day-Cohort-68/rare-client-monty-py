73cbb8fc9eeb63edb78d5df7155608958261dba1
Will MacLean <maclean.will@gmail.com>
2024-03-14 15:59:26 -0500

    done

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { getAllTags } from "../../services/TagService"
 
 
 export default function TagList() {
     const navigate = useNavigate()
-  return (
-    <div className="tagList-main-container">
-        <div className="create-tag-btn-container">
-            <button onClick={()=> {navigate('/createTag')}}>Create Tag</button>
+    const [tags, setTags] = useState([])
+
+    useEffect(() => {
+        getAllTags().then((data) => { setTags(data) })
+    }, [])
+
+    return (
+        <div>
+            <div className="posts-container">
+                <div className="posts">
+                    {tags?.map((tagObject) => {
+                        return (
+                        <div>
+                            <p className="post">{tagObject.label}</p>
+                        </div>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className="tagList-main-container">
+                <div className="create-tag-btn-container">
+                    <button onClick={()=> {navigate('/createTag')}}>Create Tag</button>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
+
