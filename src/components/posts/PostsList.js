024c92f1b8e8ff82be65c9841bb4201ff1e63dc4
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-14 15:37:04 -0500

    only approved posts will display

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -15,14 +15,16 @@ export const PostList = () => {
     <div>
       <h1>All Posts List</h1>
       {allPosts.map((post) => {
-        return (
-          <Link to={`/posts/${post.id}`} key={post.id}>
-            <p className="post">
-              Title: {post.title} Author: {post.user.first_name}{" "}
-              {post.user.last_name} Category: {post.category.label}
-            </p>
-          </Link>
-        );
+        if (post.approved === 1) {
+            return (
+              <Link to={`/posts/${post.id}`} key={post.id}>
+                <p className="post">
+                  Title: {post.title} Author: {post.user.first_name}{" "}
+                  {post.user.last_name} Category: {post.category.label}
+                </p>
+              </Link>
+            );
+          }
       })}
     </div>
   );
