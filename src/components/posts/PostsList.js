53f42d71233288509a73317d22813a3ffb6d402f
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-14 13:21:37 -0500

    created a useState with all posts

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -1,3 +1,16 @@
+import { getAllPosts } from "../../services/PostService"
+import { useState, useEffect } from "react"
+
+
 export const PostList = () => {
+const [allPosts, setAllPosts] = useState([])
+
+useEffect(() => {
+    getAllPosts().then((posts) => {
+        setAllPosts(posts)
+    })
+}, [])
+
+
     return <><h1>All Posts List</h1></>
 }
