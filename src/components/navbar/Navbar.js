93af498e3a43d73dd55fc342ad5f3329364e0608
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-14 13:26:31 -0500

    added link in navbar to posts

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,12 +11,14 @@ export const Navbar = () => {
         <div className="navbar-main">
             <ul className="navbar-list">
                 <li className="navbar-item">
-                    <Link to={"./categories"}>Category Management</Link>
+                    <Link to={"./posts"}>All Posts</Link>
                 </li>
                 <li className="navbar-item">
                     <Link to={"./myposts"}>My Posts</Link>
                 </li>
-                
+                <li className="navbar-item">
+                    <Link to={"./categories"}>Category Management</Link>
+                </li>
 
                 {localStorage.getItem("rareClient-user") ? (
                     <li className="navbar-item">
